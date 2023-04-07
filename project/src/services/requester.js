80d5819bf1c17cd548dc8433f0cdb61fc080b056
Damian Todorov <damqn_dimitrov1111@icloud.com>
2023-04-07 09:30:15 +0300

    fix requester

--- project/src/services/requester.js
+++ project/src/services/requester.js
@@ -1,54 +1,50 @@
-const requester = async (method, token, url, data) => {
-    const options = {};
-  
-    if (method !== "GET") {
-      options.method = method;
-  
-      if (data) {
-        options.headers = {
-          "content-type": "application/json",
-        };
-  
-        options.body = JSON.stringify(data);
-      }
+const requester = async (method, url, data) => {
+  const options = {};
+
+  if (method !== "GET") {
+    options.method = method;
+
+    if (data) {
+      options.headers = {
+        "content-type": "application/json",
+      };
+
+      options.body = JSON.stringify(data);
     }
-  
-    if (token) {
+  }
+  const serializedAuth = localStorage.getItem("auth");
+
+  if (serializedAuth) {
+    const auth = JSON.parse(serializedAuth);
+    if (auth.accessToken) {
       options.headers = {
         ...options.headers,
-        "X-Authorization": token,
+        "X-Authorization": auth.accessToken,
       };
     }
-  
-    const response = await fetch(url, options);
-  
-    if (response.status === 204) {
-      return {};
-    }
-  
-    const result = await response.json();
-  
-    if (!response.ok) {
-      throw result;
-    }
-  
-    return result;
   };
-  
-  export const requestFactory = (token) => {
-    if (!token) {
-      const serializedAuth = localStorage.getItem("auth");
-  
-      if (serializedAuth) {
-        const auth = JSON.parse(serializedAuth);
-        token = auth.accessToken;
-      }
-    }
-    return {
-      get: requester.bind(null, "GET", token),
-      post: requester.bind(null, "POST", token),
-      put: requester.bind(null, "PUT", token),
-      patch: requester.bind(null, "PATCH", token),
-      delete: requester.bind(null, "DELETE", token),
-    };
+
+  const response = await fetch(url, options);
+
+  if (response.status === 204) {
+    return {};
   };
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw result;
+  }
+
+  return result;
+};
+
+export const requestFactory = (token) => {
+  return {
+    get: requester.bind(null, "GET"),
+    post: requester.bind(null, "POST"),
+    put: requester.bind(null, "PUT"),
+    patch: requester.bind(null, "PATCH"),
+    delete: requester.bind(null, "DELETE"),
+  };
+};
